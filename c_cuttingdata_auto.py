7b04dea2ec1383d2759705ae86d54597698f1d57
dwh-ueno <gd.69c.0552@s.thers.ac.jp>
2023-04-26 15:57:53 +0900

    2023_04_26

--- c_cuttingdata_auto.py
+++ c_cuttingdata_auto.py
@@ -15,13 +15,10 @@ path_ueno = r"\\dwhnas1\DWH1\u_ueno\c_cuttingdata_auto"
 path_cuttingfolder = r"\\dwhnas1\DWH1\泉州電業\泉州_切断資料_自動化用"
 path_label_template_folder = r"\\dwhnas1\DWH1\u_ueno\c_cuttingdata_auto\s切断資料_雛形_保管場所"
 
-
-# 入力欄から切断資料を指定
-st.title('切断資料自動作成')
-filename = st.text_input("切断資料ファイル名（入力）")
+st.title("切断資料自動作成")
 
 # 選択欄から切断資料を指定
-sep = "\\" if os.name =="nt" else "/"
+sep = "\\" if os.name == "nt" else "/"
 file_paths = sorted(glob.glob(os.path.join(path_cuttingfolder, "*.xlsx")))
 
 filename_lst_duplication = [""]
@@ -29,9 +26,17 @@ for file_path in file_paths:
     filename = file_path.split(sep)[-1].replace(f".xlsx", "")
     filename_lst_duplication.append(filename)
 filename_list = list(dict.fromkeys(filename_lst_duplication))
-
 filename = st.selectbox("切断資料ファイル名（選択）", filename_list)
+
+# 入力欄から切断資料を指定
+if filename == "":
+    filename = st.text_input("切断資料ファイル名（入力）")
+
 path = os.path.join(path_cuttingfolder, f"{filename}.xlsx")
+
+"""
+追加で印刷するシートの選択
+"""
 
 wb = openpyxl.load_workbook(path)
 # 切断資料のシートを取得
@@ -43,7 +48,7 @@ col = st.columns(len(sheet_all))
 
 sheet_active_flag = []
 for i, sheet in enumerate(sheet_all):
-    if i==0:
+    if i == 0:
         sheet_active_flag.append(True)
     else:
         sheet_ = col[i].checkbox(label=sheet)
@@ -51,28 +56,32 @@ for i, sheet in enumerate(sheet_all):
 
 
 # 雛形選択
-path_label_template = sorted(glob.glob(os.path.join(path_label_template_folder, "*.jlb")))
+path_label_template = sorted(
+    glob.glob(os.path.join(path_label_template_folder, "*.jlb"))
+)
 label_template_lst_duplication = []
 for path_label in path_label_template:
     templatename = path_label.split(sep)[-1].replace(".jlb", "")
     label_template_lst_duplication.append(templatename)
 label_template_lst = list(dict.fromkeys(label_template_lst_duplication))
 
-label_template = st.radio("切断資料雛形", label_template_lst)
+label_template = st.radio("ラベル雛形", label_template_lst)
+label_template = os.path.join(
+    r"\\dwhnas1\DWH1\u_ueno\c_cuttingdata_auto\s切断資料_雛形_保管場所", label_template
+)
 
 
-def sheet_activate(sheet_all:List[str], path:str, sheet_activate_flag:List[bool]):
+def sheet_activate(sheet_all: List[str], path: str, sheet_activate_flag: List[bool]):
     wb = openpyxl.load_workbook(path)
     for sheet in sheet_all:
         ws = wb[sheet]
         ws.sheet_view.tabSelected = False
     wb.save(path)
     wb.close()
-    
+
     wb = openpyxl.load_workbook(path)
-    
+
     for i, active in enumerate(sheet_activate_flag):
-        print("ww", i, active)
         if active:
             ws = wb.worksheets[i]
             wb.active = i
@@ -82,49 +91,45 @@ def sheet_activate(sheet_all:List[str], path:str, sheet_activate_flag:List[bool]
     wb.save(path)
 
 
-def sheet_1(sheet_all:List[str], filename:str):
+def sheet_1(sheet_all: List[str], filename: str):
     wb = openpyxl.load_workbook(path)
     for sheet in sheet_all:
         ws = wb[sheet]
         if sheet == filename:
-            print(filename)
             ws.sheet_view.tabSelected = True
         else:
             ws.sheet_view.tabSelected = False
     wb.save(path)
     wb.close()
 
+
 # サイドバー
-st.sidebar.header('編集画面')
-edit_form = st.sidebar.form('edit_form')
-disconnection_date = edit_form.date_input('切断期日')
-order_number = edit_form.number_input('受注番号', 0,  1000000, 0)
-number_of_order = edit_form.number_input('受注数', 0,  10000, 0)
-deadline = edit_form.date_input('納期')
+st.sidebar.header("編集画面")
+edit_form = st.sidebar.form("edit_form")
+disconnection_date = edit_form.date_input("切断期日")
+order_number = edit_form.number_input("受注番号", 0, 1000000, 0)
+number_of_order = edit_form.number_input("受注数", 0, 10000, 0)
+deadline = edit_form.date_input("納期")
 push_button = edit_form.form_submit_button()
 
 
 # Excelにて切断資料を印刷する
 def PrintOut():
     win32api.ShellExecute(
-        0,
-        "print",
-        path,
-        "/c:""%s" % win32print.GetDefaultPrinter(),
-        ".",
-        0
+        0, "print", path, "/c:" "%s" % win32print.GetDefaultPrinter(), ".", 0
     )
 
+
 # ラベル編集ソフトを用いて，編集した切断資料を読み込んで印刷する
-def label_main(excel_path:str, label_template:str):
+def label_main(excel_path: str, label_template: str):
     # ラベルソフトで正常に切断資料を読み込むために，切断資料を開いて保存したのち閉じる
-    subprocess.Popen(['start', excel_path], shell=True)
+    subprocess.Popen(["start", excel_path], shell=True)
     sleep(5)
     ag.hotkey("ctrl", "s")
     sleep(1)
     ag.hotkey("alt", "f4")
     sleep(3)
-    
+
     # ラベル編集ソフトを起動する
     ag.press("win")
     sleep(1)
@@ -132,33 +137,28 @@ def label_main(excel_path:str, label_template:str):
     ag.hotkey("ctrl", "v")
     sleep(2)
     ag.press("Enter")
-    sleep(2)
-    
+    sleep(3)
+
     # 切断資料の雛形を読み込む
-    ag.press("tab", presses=3)
-    ag.write(label_template)
+    ag.press("tab", presses=3, interval=0.5)
+
     ag.press("Enter")
-    sleep(0.5)
+    sleep(1.5)
     ag.hotkey("alt", "n")
-    pyperclip.copy(os.path.join(path_ueno, "s切断資料_雛形_保管場所\s切断資料 自動化テスト用.jlb"))
+    pyperclip.copy(label_template + ".jlb")
     ag.hotkey("ctrl", "v")
     ag.press("Enter")
     sleep(2)
-    
+
     # 「データ差込・連番の設定」を選択する
-    p = ag.locateOnScreen((r"C:\label_picture\data_setting_ueno.png"),
-                          confidence=0.8
-                          )
+    p = ag.locateOnScreen((r"C:\label_picture\data_setting_ueno.png"), confidence=0.8)
     x, y = ag.center(p)
     ag.click(x, y)
     sleep(2)
-    
+
     # 編集した切断資料を読み込む
-    q = ag.locateOnScreen((r"C:\label_picture\load_data_ueno.png"),
-                          confidence=0.8
-                          )
+    q = ag.locateOnScreen((r"C:\label_picture\load_data_ueno.png"), confidence=0.8)
     x, y = ag.center(q)
-    print(x,y)
     ag.click(x, y)
     ag.hotkey("alt", "t")
     sleep(1)
@@ -182,7 +182,7 @@ def label_main(excel_path:str, label_template:str):
     sleep(1)
     ag.press("Enter")
     sleep(1)
-    
+
     # プリンタを設定する
     ag.hotkey("ctrl", "p")
     sleep(1)
@@ -200,11 +200,20 @@ def label_main(excel_path:str, label_template:str):
     sleep(1)
     ag.press("Enter")
     sleep(1)
-    
+
+
+def label_print():
+    print("a")
+    print(ag.getWindowsWithTitle("ラベルマイティプレミアム"))
+    labelprinter_window = ag.getWindowsWithTitle("ラベルマイティプレミアム")[0]
+    labelprinter_window.activate()
+
     # 切断資料を印刷する
     ag.hotkey("shift", "tab")
     sleep(1)
     ag.press("Enter")
+
+    """
     sleep(10)
     
     # ラベル編集ソフトを閉じる
@@ -212,6 +221,7 @@ def label_main(excel_path:str, label_template:str):
     sleep(0.5)
     ag.press("n")
     st.warning("ラベルを印刷しました！")
+    """
 
 
 # Streamlitでsubmitした後の処理
@@ -223,10 +233,10 @@ if push_button:
     wb = openpyxl.load_workbook(path)
     sheet_main = wb[filename]
     for i in range(1, 5):
-        sheet_main.row_dimensions[i].hidden=False
+        sheet_main.row_dimensions[i].hidden = False
     wb.save(path)
     wb.close()
-    
+
     # 切断期日，受注番号，受注数，納期を切断資料に反映する
     sheet_main["O1"] = disconnection_date.strftime("%Y/%m/%d")
     sheet_main["O2"].value = order_number
@@ -234,38 +244,33 @@ if push_button:
     sheet_main["O4"] = deadline.strftime("%Y/%m/%d")
     wb.save(path)
     wb.close()
-    
+
     # 1-4行目を表示させたまま切断資料を印刷するために．切断資料を開いて保存したのち閉じる
-    subprocess.Popen(['start', path], shell=True)
+    subprocess.Popen(["start", path], shell=True)
     sleep(5)
     ag.hotkey("ctrl", "s")
     sleep(1)
     ag.hotkey("alt", "f4")
     sleep(5)
-    
+
     # 選択したシートをアクティブ化する
     sheet_activate(sheet_all, path, sheet_active_flag)
-    
+
     # 切断資料を印刷する
-    PrintOut()
+    # PrintOut()
     sleep(5)
     st.warning("切断資料を印刷しました！")
-    
+
     # 1-4行目を非表示にする
     wb = openpyxl.load_workbook(path)
     sheet_main = wb[filename]
     for i in range(1, 5):
-        sheet_main.row_dimensions[i].hidden=True
+        sheet_main.row_dimensions[i].hidden = True
     wb.save(path)
     wb.close()
-    
+
     # 切断資料のみアクティブ化する
     sheet_1(sheet_all, filename)
-    
+
     # ラベルプリンタを印刷する
     label_main(path, label_template)
-    
-
-
-
-# 雛形を入れるフォルダ，切断資料を入れるフォルダのパスへの変更が必要
